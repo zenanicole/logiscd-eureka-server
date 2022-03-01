f7a81c2e3302d2430c25f889e875f88aafa10d73
Nicole <zenanicole1412@gmail.com>
2022-02-28 20:50:13 -0700

    first commit

---
+++ src/test/java/com/logiscdweb/logiscdwebeurekaserver/LogiscdwebEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiscdweb.logiscdwebeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiscdwebEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
